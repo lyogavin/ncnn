43485a60fbcf15f740e68c17244d8e3687cbeb81
Yu Li <lyo.gavin@gmail.com>
2020-05-09 12:27:57 -0700

    fix namespace

--- tests/test_convolution.cpp
+++ tests/test_convolution.cpp
@@ -12,13 +12,15 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+
+#include <QPULib.h>
+
 #include "testutil.h"
 
 #include "layer/convolution.h"
 
 #include "layer_type.h"
 
-#include <QPULib.h>
 
 namespace ncnn {
 #include "layer/arm/convolution_1x1.h"
