15e0457da6b7bd8ad090abed01ba9c1c89bcd365
Yu Li <lyo.gavin@gmail.com>
2020-05-18 15:21:11 -0700

    log diff

--- src/layer/arm/convolution_1x1.h
+++ src/layer/arm/convolution_1x1.h
@@ -128,7 +128,7 @@ static void conv1x1s1_sgemm_qpu(const Mat& bottom_blob, Mat& top_blob, const Mat
 
 }
 
-static void conv1x1s1_sgemm_neon_original(const Mat& bottom_blob, Mat& top_blob, const Mat& kernel, const Mat& _bias, const Option& opt)
+static void conv1x1s1_sgemm_neon(const Mat& bottom_blob, Mat& top_blob, const Mat& kernel, const Mat& _bias, const Option& opt)
 {
     if (!g_useqpu) {
         return conv1x1s1_sgemm_neon_original(bottom_blob, top_blob, kernel, _bias, opt);
